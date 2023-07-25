9f12ecca6aef6ad48e7fd02cb1fec00b115d78a8
REV3NT3CH <rev3nt3ch@gmail.com>
2023-07-25 10:14:28 -0700

    floral: AlphaDroid Bringup

--- lineage_flame.mk
+++ lineage_flame.mk
@@ -4,14 +4,49 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common AlphaDroid stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/coral/aosp_flame.mk)
 
 include device/google/coral/flame/device-lineage.mk
+
+# AlphaDroid Xtras
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_PIXEL_FRAMEWORK := true
+
+# GAPPS configs
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_GOOGLE_APP := true
+TARGET_INCLUDE_GOOGLE_CAMERA := true
+TARGET_INCLUDE_GOOGLE_CALCULATOR := true
+TARGET_INCLUDE_GOOGLE_CALENDAR := true
+TARGET_INCLUDE_GOOGLE_CHROME := true
+TARGET_INCLUDE_GOOGLE_DESKCLOCK := true
+TARGET_INCLUDE_GOOGLE_DRIVE := true
+TARGET_INCLUDE_GMAIL := true
+TARGET_INCLUDE_GOOGLE_MAPS := true
+TARGET_INCLUDE_GOOGLE_MARKUP := true
+TARGET_INCLUDE_GOOGLE_PHOTOS := true
+TARGET_INCLUDE_GOOGLE_SETUP := true
+
+# Use Google Telephony pack (Dialer, Contacts, Messaging) on GAPPS builds
+# Default = true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Debugging
+# Default = false
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := REV3NT3CH
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
